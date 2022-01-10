971cf42b261ea041ea9c6ade33c57ec1eeb793fd
chensicheng <chensichengmalu@gmail.com>
2022-01-11 01:06:38 +0800

    init

---
+++ precache-manifest.ef4af0067108fc3cedfe5f33bb9824c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aefb6f70bb8ab90d669a15c0de997bbd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86afda4e6d421259cbc0",
+    "url": "/static/css/2.e10cf4f1.chunk.css"
+  },
+  {
+    "revision": "8fa4a7af08adaacb7160",
+    "url": "/static/css/main.f4cf0f37.chunk.css"
+  },
+  {
+    "revision": "86afda4e6d421259cbc0",
+    "url": "/static/js/2.c040b6a2.chunk.js"
+  },
+  {
+    "revision": "8fa4a7af08adaacb7160",
+    "url": "/static/js/main.17e0a756.chunk.js"
+  },
+  {
+    "revision": "353ddccecafb8f7190ef",
+    "url": "/static/js/runtime~main.477b257c.js"
+  }
+]);
